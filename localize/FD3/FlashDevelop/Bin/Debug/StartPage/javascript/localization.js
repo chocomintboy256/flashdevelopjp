4dde90df6cbcb6209c5c08caa803d79e2a0ffa1b
matsumos <matsumos@d27b381a-bb1e-11de-82ee-51df2961d3cf>
2009-10-19 07:02:37 +0000

    ローカライズ用データ

---
+++ localize/FD3/FlashDevelop/Bin/Debug/StartPage/javascript/localization.js
@@ -0,0 +1,60 @@
+﻿/**
+* Locale strings for different languages.
+*/
+var locale = new Object();
+
+/**
+* Locale strings for English.
+*/
+locale["en_US.mainLinkHome"] = "Home";
+locale["en_US.mainLinkDocs"] = "Documentation";
+locale["en_US.mainLinkNotes"] = "Release Notes";
+locale["en_US.mainLinkAbout"] = "About";
+locale["en_US.createNewProject"] = "New Project...";
+locale["en_US.openProjectDialog"] = "Open Project...";
+locale["en_US.recentProjectsTitle"] = "Recent Projects";
+locale["en_US.loadingRecentProjects"] = "Loading recent projects...";
+locale["en_US.recentProjectsNotFound"] = "No recent projects found.";
+locale["en_US.versionInfoTitle"] = "Latest Version";
+locale["en_US.loadingVersionInfo"] = "Loading version info...";
+locale["en_US.versionInfoNotAvailable"] = "Version info not available.";
+locale["en_US.yourVersionIsOutOfDate"] = "Your version ({0}) is out of date.";
+locale["en_US.downloadVersionHere"] = "Download here!";
+locale["en_US.loadingRssFeed"] = "Loading RSS feed...";
+locale["en_US.rssFeedNotAvailable"] = "RSS feed not available.";
+locale["en_US.projectTypeFDP"] = "Unable to determine";
+locale["en_US.projectTypeAS2"] = "ActionScript 2 Project";
+locale["en_US.projectTypeAS3"] = "ActionScript 3 Project";
+locale["en_US.projectTypeHaXe"] = "HaXe Project";
+locale["en_US.projectTypeUnknown"] = "Unknown";
+locale["en_US.tooltipTextModified"] = "Modified:";
+locale["en_US.tooltipTextCreated"] = "Created:";
+locale["en_US.tooltipTextType"] = "Type:";
+
+/**
+* Locale strings for Japanese.
+*/
+locale["ja_JP.mainLinkHome"] = "ホーム";
+locale["ja_JP.mainLinkDocs"] = "ドキュメント";
+locale["ja_JP.mainLinkNotes"] = "リリースノート";
+locale["ja_JP.mainLinkAbout"] = "FlashDevelopについて";
+locale["ja_JP.createNewProject"] = "新しいプロジェクト...";
+locale["ja_JP.openProjectDialog"] = "プロジェクトを開く...";
+locale["ja_JP.recentProjectsTitle"] = "最近開いたプロジェクト";
+locale["ja_JP.loadingRecentProjects"] = "最近開いたプロジェクトを読込中...";
+locale["ja_JP.recentProjectsNotFound"] = "最近開いたプロジェクトはありません。";
+locale["ja_JP.versionInfoTitle"] = "最新版";
+locale["ja_JP.loadingVersionInfo"] = "バージョン情報を読込中...";
+locale["ja_JP.versionInfoNotAvailable"] = "バージョン情報はありません.";
+locale["ja_JP.yourVersionIsOutOfDate"] = "お使いのFlashDevelop ({0}) は古いバージョンです。";
+locale["ja_JP.downloadVersionHere"] = "ダウンロード";
+locale["ja_JP.loadingRssFeed"] = "フィードを読込中...";
+locale["ja_JP.rssFeedNotAvailable"] = "フィードはありません。";
+locale["ja_JP.projectTypeFDP"] = "";
+locale["ja_JP.projectTypeAS2"] = "ActionScript 2 プロジェクト";
+locale["ja_JP.projectTypeAS3"] = "ActionScript 3 プロジェクト";
+locale["ja_JP.projectTypeHaXe"] = "HaXe プロジェクト";
+locale["ja_JP.projectTypeUnknown"] = "不明";
+locale["ja_JP.tooltipTextModified"] = "変更:";
+locale["ja_JP.tooltipTextCreated"] = "作成:";
+locale["ja_JP.tooltipTextType"] = "タイプ:";
---
+++ localize/FD3/PluginCore/PluginCore/Localization/LocaleVersion.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PluginCore.Localization
+{
+    public enum LocaleVersion
+    {
+        [StringValue("en_US")]
+        en_US = 0,
+
+        [StringValue("ja_JP")]
+        ja_JP = 1
+    }
+
+}
